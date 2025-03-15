49478ad36c06c1742bf76f92d99d89694a9d9c4a
Esdeath <39652390+notesdeath@users.noreply.github.com>
2025-03-15 06:18:18 +0100

    Add files via upload

--- output/client_dll.cs
+++ output/client_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-03-13 00:20:59.138749100 UTC
+// 2025-03-15 05:15:08.478754900 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: client.dll
